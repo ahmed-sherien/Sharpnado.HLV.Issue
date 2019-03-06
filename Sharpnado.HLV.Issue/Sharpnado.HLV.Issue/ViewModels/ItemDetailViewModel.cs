d43aee8fc2fa3d720588c4d1b6c8c965c89ec8c3
Ahmed Sherien <eng.a.sherien@gmail.com>
2019-03-06 11:36:44 +0200

    Add project files.

---
+++ Sharpnado.HLV.Issue/Sharpnado.HLV.Issue/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Sharpnado.HLV.Issue.Models;
+
+namespace Sharpnado.HLV.Issue.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ Sharpnado.HLV.Issue/Sharpnado.HLV.Issue/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Sharpnado.HLV.Issue.Models;
+using Sharpnado.HLV.Issue.ViewModels;
+
+namespace Sharpnado.HLV.Issue.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+
+            var item = new Item
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemDetailViewModel(item);
+            BindingContext = viewModel;
+        }
+    }
+}
